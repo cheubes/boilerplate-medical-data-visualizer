48a81d9b7ce2a06c7eba2d8a181ec65a13a53cf2
cheubes <christophe@heubes.com>
2021-02-15 14:41:52 +0100

    Pass test_heat_map_labels and modified test_heat_map_values.

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -23,34 +23,40 @@ def draw_cat_plot():
     df_cat = df_cat.groupby(['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke', 'cardio']).size().rename('total').reset_index().melt(['total', 'cardio'])
 
     # Draw the catplot with 'sns.catplot()'
-    fig=plt.gcf()
+    plt.figure('cat')
     sns.catplot(data = df_cat, x='variable', y='total', hue='value', col='cardio', kind="bar", ci = None)
 
     # Do not modify the next two lines
-    fig.savefig('catplot.png')
-    return fig
+    plt.gcf().savefig('catplot.png')
+    return plt.gcf()
 
 
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df.copy(deep=True)
+    df_heat = df_heat.loc[df_heat['ap_lo'] <= df_heat['ap_hi']]
+    df_heat = df_heat.loc[df_heat['height'] >= df_heat['height'].quantile(0.025)]
+    df_heat = df_heat.loc[df_heat['height'] <= df_heat['height'].quantile(0.975)]
+    df_heat = df_heat.loc[df_heat['weight'] >= df_heat['weight'].quantile(0.025)]
+    df_heat = df_heat.loc[df_heat['weight'] <= df_heat['weight'].quantile(0.975)]
+
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr()
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    plt.figure('heat')
 
     # Draw the heatmap with 'sns.heatmap()'
-
+    sns.heatmap(corr, mask=mask,annot=True, fmt='.1f', vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={'shrink': .5})
 
 
     # Do not modify the next two lines
-    fig.savefig('heatmap.png')
-    return fig
+    plt.gcf().savefig('heatmap.png')
+    return plt.gcf()
